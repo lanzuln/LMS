a647d41cdc32b13ed60a3c7e14b0afe746e416cf
lanzuln <lanzu.dev@gmail.com>
2024-02-17 00:46:39 +0600

    initial frontend setup done

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,16 @@
 
 use App\Http\Controllers\backend\AdminController;
 use App\Http\Controllers\backend\instructor\InstructorController;
+use App\Http\Controllers\frontend\frontendIndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// ================== frontend ===========
+Route::get('/', [frontendIndexController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
