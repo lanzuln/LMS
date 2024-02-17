6db6f7954313e88e0a35a3f95a9c649c1c3c8bfe
lanzuln <lanzu.dev@gmail.com>
2024-02-17 17:20:21 +0600

    home page templeting done

--- resources/views/frontend/components/home/hero.blade.php
+++ resources/views/frontend/components/home/hero.blade.php
@@ -1,1 +1,129 @@
 
+<section class="hero-area">
+    <div class="hero-slider owl-action-styled owl-loaded owl-carousel">
+        <!-- end hero-slider-item -->
+        <!-- end hero-slider-item -->
+        <!-- end hero-slider-item -->
+    <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-3806px, 0px, 0px); transition: all 0s ease 0s; width: 13321px;"><div class="owl-item cloned" style="width: 1903px;"><div class="hero-slider-item hero-bg-2">
+            <div class="container">
+                <div class="hero-content text-center">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Join Aduca &amp; Get <br> Your Free Courses!</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Get Started <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div></div><div class="owl-item cloned" style="width: 1903px;"><div class="hero-slider-item hero-bg-3">
+            <div class="container">
+                <div class="hero-content text-right">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Learn Anything, <br> Anytime, Anywhere</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div>
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-end">
+                        <a href="#" class="btn-text video-play-btn mr-4 mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            <i class="la la-play icon-btn mr-2"></i>Watch Preview
+                        </a>
+                        <a href="admission.html" class="btn theme-btn mb-4"><i class="la la-arrow-left icon mr-1"></i>Get Enrolled </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div></div><div class="owl-item active" style="width: 1903px;"><div class="hero-slider-item hero-bg-1">
+
+            <div class="container">
+                <div class="hero-content">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">We Help You Learn <br> What You Love</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1">
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Join with Us <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- end container -->
+        </div></div><div class="owl-item" style="width: 1903px;"><div class="hero-slider-item hero-bg-2">
+            <div class="container">
+                <div class="hero-content text-center">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Join Aduca &amp; Get <br> Your Free Courses!</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Get Started <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div></div><div class="owl-item" style="width: 1903px;"><div class="hero-slider-item hero-bg-3">
+            <div class="container">
+                <div class="hero-content text-right">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Learn Anything, <br> Anytime, Anywhere</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div>
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-end">
+                        <a href="#" class="btn-text video-play-btn mr-4 mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            <i class="la la-play icon-btn mr-2"></i>Watch Preview
+                        </a>
+                        <a href="admission.html" class="btn theme-btn mb-4"><i class="la la-arrow-left icon mr-1"></i>Get Enrolled </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div></div><div class="owl-item cloned" style="width: 1903px;"><div class="hero-slider-item hero-bg-1">
+
+            <div class="container">
+                <div class="hero-content">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">We Help You Learn <br> What You Love</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1">
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Join with Us <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- end container -->
+        </div></div><div class="owl-item cloned" style="width: 1903px;"><div class="hero-slider-item hero-bg-2">
+            <div class="container">
+                <div class="hero-content text-center">
+                    <div class="section-heading">
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Join Aduca &amp; Get <br> Your Free Courses!</h2>
+                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
+                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        </p>
+                    </div><!-- end section-heading -->
+                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Get Started <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox="" data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        </a>
+                    </div><!-- end hero-btn-box -->
+                </div><!-- end hero-content -->
+            </div><!-- container -->
+        </div></div></div></div><div class="owl-nav"><div class="owl-prev"><i class="la la-angle-left"></i></div><div class="owl-next"><i class="la la-angle-right"></i></div></div><div class="owl-dots"><div class="owl-dot active"><span></span></div><div class="owl-dot"><span></span></div><div class="owl-dot"><span></span></div></div></div><!-- end hero-slide -->
+</section>
